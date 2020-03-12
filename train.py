7dd15e444b7144e7345bad0b79c7c063a759aeb7
pranand <pranand@pranand-11804.pods.pranand.svc.cluster.local>
2020-03-11 19:48:57 -0700

    Early stopping + saving model

--- train.py
+++ train.py
@@ -6,11 +6,10 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from data_loader import get_loader
-from models import VqaModel, SANModel
-
+from models import VqaModel
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# torch.backends.cudnn.enabled = False
+
 
 def main(args):
 
@@ -30,16 +29,7 @@ def main(args):
     ans_vocab_size = data_loader['train'].dataset.ans_vocab.vocab_size
     ans_unk_idx = data_loader['train'].dataset.ans_vocab.unk2idx
 
-#     model = VqaModel(
-#         embed_size=args.embed_size,
-#         qst_vocab_size=qst_vocab_size,
-#         ans_vocab_size=ans_vocab_size,
-#         word_embed_size=args.word_embed_size,
-#         num_layers=args.num_layers,
-#         hidden_size=args.hidden_size).to(device)
-
-
-    model = SANModel(
+    model = VqaModel(
         embed_size=args.embed_size,
         qst_vocab_size=qst_vocab_size,
         ans_vocab_size=ans_vocab_size,
@@ -48,23 +38,24 @@ def main(args):
         hidden_size=args.hidden_size).to(device)
 
     criterion = nn.CrossEntropyLoss()
-    
-#     params = list(model.img_encoder.fc.parameters()) \
-#         + list(model.qst_encoder.parameters()) \
-#         + list(model.fc1.parameters()) \
-#         + list(model.fc2.parameters())
-    
-    # resume training
-    if args.resume_epoch!=0:  
-        load_var = torch.load('./models/model-epoch-{:02d}.pt'.format(args.resume_epoch))
-        assert load_var['epoch'] == args.resume_epoch
-        model = load_var['model']
-        torch.cuda.empty_cache()
 
-    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    params = list(model.img_encoder.fc.parameters()) \
+        + list(model.qst_encoder.parameters()) \
+        + list(model.fc1.parameters()) \
+        + list(model.fc2.parameters())
+
+    optimizer = optim.Adam(params, lr=args.learning_rate)
     scheduler = lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
 
-    for epoch in range(args.resume_epoch, args.num_epochs):
+    #callbacks = [EarlyStopping(monitor='val_loss', patience=5)]
+    #model.set_callbacks(callbacks)
+    early_stop_threshold = 5
+    best_loss = 99999
+    val_increase_count = 0
+    stop_training = False
+    prev_loss = 9999
+
+    for epoch in range(args.num_epochs):
 
         for phase in ['train', 'valid']:
 
@@ -80,9 +71,7 @@ def main(args):
                 model.eval()
 
             for batch_idx, batch_sample in enumerate(data_loader[phase]):
-                #if batch_idx == 5:
-                 #   break
-                
+
                 image = batch_sample['image'].to(device)
                 question = batch_sample['question'].to(device)
                 label = batch_sample['answer_label'].to(device)
@@ -110,9 +99,9 @@ def main(args):
                 running_corr_exp2 += torch.stack([(ans == pred_exp2.cpu()) for ans in multi_choice]).any(dim=0).sum()
 
                 # Print the average loss in a mini-batch.
-#                 if batch_idx % 100 == 0:
-                print('| {} SET | Epoch [{:02d}/{:02d}], Step [{:04d}/{:04d}], Loss: {:.4f}'
-                          .format(phase.upper(), epoch+1, args.num_epochs, batch_idx, int(batch_step_size), loss.item()), end='\r')
+                if batch_idx % 100 == 0:
+                    print('| {} SET | Epoch [{:02d}/{:02d}], Step [{:04d}/{:04d}], Loss: {:.4f}'
+                          .format(phase.upper(), epoch+1, args.num_epochs, batch_idx, int(batch_step_size), loss.item()))
 
             # Print the average loss and accuracy in an epoch.
             epoch_loss = running_loss / batch_step_size
@@ -130,17 +119,31 @@ def main(args):
                         + str(epoch_acc_exp1.item()) + '\t'
                         + str(epoch_acc_exp2.item()))
 
+            if phase == 'valid':
+                if epoch_loss < best_loss:
+                    best_loss = epoch_loss
+                    torch.save(model, os.path.join(args.model_dir, 'best_model.ckpt'))
+                if epoch_loss > prev_loss:
+                    val_increase_count += 1
+                else:
+                    val_increase_count = 0
+                if val_increase_count >= early_stop_threshold:
+                    stop_training = True
+                prev_loss = epoch_loss
         # Save the model check points.
         if (epoch+1) % args.save_step == 0:
-            torch.save({'epoch': epoch+1, 'model': model},
-                       os.path.join(args.model_dir, 'model-epoch-{:02d}.pt'.format(epoch+1)))
+            #torch.save({'epoch': epoch+1, 'state_dict': model.state_dict()},
+            torch.save(model,
+                       os.path.join(args.model_dir, 'model-epoch-{:02d}.ckpt'.format(epoch+1)))
 
+        if stop_training:
+            break
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--input_dir', type=str, default='../COCO-2015/resized_images',
+    parser.add_argument('--input_dir', type=str, default='./datasets',
                         help='input directory for visual question answering.')
 
     parser.add_argument('--log_dir', type=str, default='./logs',
@@ -182,7 +185,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_epochs', type=int, default=30,
                         help='number of epochs.')
 
-    parser.add_argument('--batch_size', type=int, default=256,
+    parser.add_argument('--batch_size', type=int, default=64,
                         help='batch_size.')
 
     parser.add_argument('--num_workers', type=int, default=8,
@@ -190,9 +193,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--save_step', type=int, default=1,
                         help='save step of model.')
-    
-    parser.add_argument('--resume_epoch', type=int, default = 0,
-                        help='load saved model from which epoch')
 
     args = parser.parse_args()
 
