3a7fb0a1a40a0f0190a259723e190e52961605d1
SatyamGaba <satyamgb321@gmail.com>
2020-03-12 18:27:39 -0700

    Updated test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import argparse
+#from utilities import text_helper
 import cv2
+from PIL import Image
 import numpy as np
 import torch
 import torch.nn as nn
+import torchvision
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from data_loader import get_loader
@@ -11,21 +14,76 @@ from models import VqaModel, SANModel
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+def load_str_list(fname):
+    with open(fname) as f:
+        lines = f.readlines()
+    lines = [l.strip() for l in lines]
+    return lines
 
+
+qst_vocab = load_str_list("datasets/vocab_questions.txt")
+ans_vocab = load_str_list("datasets/vocab_answers.txt")
+word2idx_dict = {w:n_w for n_w, w in enumerate(qst_vocab)}
+unk2idx = word2idx_dict['<unk>'] if '<unk>' in word2idx_dict else None
+vocab_size = len(qst_vocab)
+
+def load_image(image_path, transform=None):
+    image = Image.open(image_path).convert('RGB')
+    image = image.resize([224, 224], Image.LANCZOS)
+    
+    if transform is not None:
+        image = transform(image).unsqueeze(0)
+    
+    return image
+
+
+
+def word2idx(w):
+    if w in word2idx_dict:
+        return word2idx_dict[w]
+    elif unk2idx is not None:
+         return unk2idx
+ 
+    else:
+        raise ValueError('word %s not in dictionary (while dictionary does not contain <unk>)' % w)
+        
 def main(args):
-    image = args.image_path.to(device)
-    question = args.question.to(device)
-    image = cv2.resize(image, size=224, interpolation = cv2.INTER_AREA)
-    #resize_image(image, size = 224)
+     
+ 
+   
+    image = cv2.imread(args.image_path)
+    image = cv2.resize(image, dsize=(224,224), interpolation = cv2.INTER_AREA)
+    image = torch.from_numpy(image).float()
+    image = image.to(device)
+    image = image.unsqueeze(dim=0)
+    image = image.view(1,3,224,224)
+    
+    max_qst_length=30
+    
+    question = args.question
+    q_list = list(question.split(" "))
+    print(q_list)
+    
+    idx = 'valid'
+    qst2idc = np.array([word2idx('<pad>')] * max_qst_length)  # padded with '<pad>' in 'ans_vocab'
+    qst2idc[:len(q_list)] = [word2idx(w) for w in q_list]
+
+    question = qst2idc
+    question = torch.from_numpy(question).long()
+    
+    question = question.to(device)
+    question = question.unsqueeze(dim=0)
     model = torch.load(args.saved_model)
+    model = model.to(device)
     #torch.cuda.empty_cache()
     model.eval()
     output = model(image, question)
     _, pred_exp1 = torch.max(output, 1)
-    print(pred_exp1)
+    print(ans_vocab[pred_exp1.item()])
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
+   
     parser = argparse.ArgumentParser()
     parser.add_argument('--image_path', type = str, required=True)
     parser.add_argument('--question', type = str, required=True)
